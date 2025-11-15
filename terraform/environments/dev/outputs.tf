2df3377b9a3f1ef5e2fc5e233fedcc6c400e4b8d
Namdev Rathod <namdev.it@gmail.com>
2025-11-15 19:45:45 +0530

    GitHub Action

--- terraform/environments/dev/outputs.tf
+++ terraform/environments/dev/outputs.tf
@@ -3,11 +3,6 @@ output "cluster_name" {
   value       = module.eks.cluster_name
 }
 
-output "cluster_endpoint" {
-  description = "EKS Cluster API Server Endpoint"
-  value       = module.eks.cluster_endpoint
-}
-
 output "vpc_id" {
   description = "Created VPC ID"
   value       = module.vpc.vpc_id
