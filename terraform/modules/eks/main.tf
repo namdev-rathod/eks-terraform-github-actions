4831d5d05bc893c6f5a7ac30e01e3df9278785c7
Namdev Rathod <namdev.it@gmail.com>
2025-11-16 05:49:53 +0530

    GitHub Action

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -19,7 +19,7 @@ module "eks" {
   version = "~> 20.8"
 
   cluster_name    = var.cluster_name
-  cluster_version = "1.33"
+  cluster_version = "1.34"
   subnet_ids      = var.private_subnets
   vpc_id          = var.vpc_id
 
@@ -27,6 +27,7 @@ module "eks" {
     devops_nodes = {
       name           = var.node_group_name
       instance_types = ["t4g.medium"]
+      ami_type       = "AL2023_ARM_64_STANDARD"
       min_size       = 1
       max_size       = 2
       desired_size   = 1
